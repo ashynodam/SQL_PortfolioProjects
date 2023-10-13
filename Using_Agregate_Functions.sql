1a5c5687035a274ee44ca499db0cf466cc334cba
Gbenga Akindeko <104626826+ashynodam@users.noreply.github.com>
2023-10-13 14:05:57 +0100

    Update Using_Agregate_Functions.sql

--- Using_Agregate_Functions.sql
+++ Using_Agregate_Functions.sql
@@ -25,44 +25,38 @@ INSERT INTO gerrard  Values
 (11, 'Mustapha Ahmed', '2016-09-01', 24, 3.9, '2016-09-01 15:11:20', 'Statistics', 27110),
 (12, 'Femi Akin', '2017-09-01', 24, NULL, '2017-09-01 09:11:32', 'Physics', 26410)
 
-
-
---COUNT FN
+--COUNT Function
 
 SELECT COUNT(*)
 FROM gerrard
 WHERE amount_paid > 28000;
 
 
---MAXIMUM FN
+--MAXIMUM Function
 
 SELECT MAX(age)
 FROM gerrard;
-
-
+-
 SELECT MAX(age)
 FROM gerrard
 WHERE department = 'Biochemistry';
 
 
---MINIMUM FN
+--MINIMUM Function
 
 SELECT MIN(age)
 FROM gerrard
 WHERE department = 'Biochemistry';
-
-
+-
 SELECT MIN(age)
 FROM gerrard
 WHERE department = 'Statistics';
 
 
---AVERAGE FN
+--AVERAGE Function
 
 SELECT AVG(amount_paid)
 FROM gerrard;
-
-
 -
 SELECT ROUND (AVG(amount_paid), 2)
 FROM gerrard;
@@ -73,13 +67,11 @@ FROM gerrard
 WHERE admitted_at = '2015-09-01';
 
 
---SUM FN
+--SUM Function
 
 SELECT SUM(amount_paid)
 FROM gerrard;
-
-
-
+-
 SELECT SUM(amount_paid)
 FROM gerrard
 WHERE department = 'Statistics';
@@ -136,14 +128,11 @@ ORDER BY sum(amount_paid) ASC;
 
 
 --HAVING CLAUSE---------------
-
 --How many transactions occurred per department, where the count is more than 2.
 
 SELECT department, COUNT(*)
 FROM gerrard
 GROUP BY department;
-
-
 
 SELECT department, COUNT(*)
 FROM gerrard
@@ -165,131 +154,3 @@ WHERE admitted_at > '2015-09-01'
 GROUP BY department
 HAVING SUM(amount_paid) > 30000
 ORDER BY SUM(amount_paid);
-
---HAVING VS WHERE
-
-
---JOINS
-
-ALIASES (AS)---
-
-SELECT admitted_at AS date
-FROM gerrard;
--
-SELECT S.name, S.grade
-FROM gerrard AS S;
--
-SELECT department, COUNT(*) AS number
-FROM gerrard
-GROUP BY department;
-
-INNER JOIN---
-
-
-
-CREATE TABLE project (
-	id INTEGER,
-    student_id INTEGER,
-    title VARCHAR(60)
-    );
-
-INSERT INTO project (id, student_id, title)
-VALUES (1, 2, 'Bioinformatics');
-
-INSERT INTO project (id, student_id, title)
-VALUES (2, 1, 'Cell Biology');
-
-INSERT INTO project (id, student_id, title)
-VALUES (3, 4, 'Fluid Mechanics');
-
-INSERT INTO project (id, student_id, title)
-VALUES (4, 6, 'Magnetism');
-
-INSERT INTO project (id, student_id, title)
-VALUES (5, 8, 'Electronics');
-
-INSERT INTO project (id, student_id, title)
-VALUES (6, 7, 'Mass Media');
-
-INSERT INTO project (id, student_id, title)
-VALUES (7, 5, 'Genetics');
-
-INSERT INTO project (id, student_id, title)
-VALUES (8, 3, 'Youth Culture');
-
-INSERT INTO project (id, student_id, title)
-VALUES (9, 15, 'Class Conflict');
-
-INSERT INTO project (id, student_id, title)
-VALUES (10, 16, 'Instrumentation');
-
-There is a new table, "Project Table" this table contains information on students that completed and submitted their project for review.
-Return the results of students who registered (in prooject table) and submitted their projects for review.
-
-MERGING TABLES
-
-SELECT *
-FROM gerrard
-INNER JOIN project
-ON gerrard.student_id = project.student_id;
--
-SELECT gerrard.student_id, name, title
-FROM gerrard 
-INNER JOIN project 
-ON gerrard.student_id = project.student_id;
--
-SELECT gerrard.student_id, name, title, department
-FROM gerrard 
-INNER JOIN project 
-ON gerrard.student_id = project.student_id;
-
----
-LEFT JOIN
-
-Retrun results for all students who submitted a project and those who didn't.
-Filter the results to highlight those who did not submit.
-
-SELECT gerrard.student_id, name, department, title
-FROM gerrard
-LEFT JOIN project
-ON gerrard.student_id = project.student_id;
--
-SELECT gerrard.student_id, name, department, title
-FROM gerrard
-LEFT JOIN project
-ON gerrard.student_id = project.student_id
-WHERE project.student_id IS NULL;
--
-SELECT gerrard.student_id, name, department, title, project.student_id
-FROM gerrard
-LEFT JOIN project
-ON gerrard.student_id = project.student_id
-WHERE project.student_id IS NULL;
-
-We have some students who submitted projects, but didn't complete the student's registration (no name in students table).
-Return results for this.
-
-SELECT gerrard.student_id, name, department, title, project.student_id
-FROM project
-LEFT JOIN gerrard
-ON gerrard.student_id = project.student_id;
-
----
-RIGHT JOIN
-
-Return results for all studets who submitted a project and those who didn't. Filter the results to hihglight those who did not submit.
-
-SELECT gerrard.student_id, name, department, title
-FROM project
-RIGHT JOIN gerrard
-ON gerrard.student_id = project.student_id;
-
-We have some students who submitted project, but didn't complete the students (no name in students table). Return results for this.
-
-SELECT gerrard.student_id, name, department, title
-FROM gerrard
-RIGHT JOIN project
-ON gerrard.student_id = project.student_id;
-
----
-SUBQUERY--------------------------------------------------------
